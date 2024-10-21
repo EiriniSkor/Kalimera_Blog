a8f684e9a728be32d4da26f9a01e9c4fc5deee4d
EiriniSkor <skordeli.eirini@gmail.com>
2024-10-21 16:14:36 +0200

    Set up registration, login and logout function

--- kalimera/urls.py
+++ kalimera/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),  # The admin site
     path('', include('mainapp.urls')),  # Include app-specific URLs
     path('accounts/', include('django.contrib.auth.urls')),  # Django's built-in authentication system
